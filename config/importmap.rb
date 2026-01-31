c54c244a96b2a54b299d606de602d2f12e690606
Maks Goncharenko <maksgoncharenko@Makss-MacBook-Pro.local>
2026-02-01 02:33:19 +0500

    Second Try

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,4 @@
-ppin "application", preload: true
+pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
