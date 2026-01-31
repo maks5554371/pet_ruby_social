8669cba93496ad66674d3f5c238ae65314a94bb0
Maks Goncharenko <maksgoncharenko@Makss-MacBook-Pro.local>
2026-02-01 02:32:15 +0500

    Second Try

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,9 @@
 // app/javascript/controllers/index.js
 import { application } from "./application";
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
+import HelloController from "./hello_controller";
+import MessageFormController from "./message_form_controller";
 
+application.register("hello", HelloController);
+application.register("message-form", MessageFormController);
 eagerLoadControllersFrom("controllers", application);
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,12 +1,13 @@
-# config/importmap.rb
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+ppin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # Для inline скриптов нужен только ActionCable
 pin "@rails/actioncable", to: "actioncable.esm.js"
 
-# Закомментируйте эту строку:
-# pin_all_from "app/javascript/channels", under: "channels"
+pin "controllers/application", to: "controllers/application.js"
+pin "controllers/hello_controller", to: "controllers/hello_controller.js"
+pin "controllers/message_form_controller", to: "controllers/message_form_controller.js"
+
